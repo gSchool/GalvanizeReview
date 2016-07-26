f6fd1ca17a7bd9a33e341539fe684df4a3342fa2
Mathew Brown <matt@mattbrown.com>
2016-07-26 09:31:32 -0600

    Updating Readme

--- README.md
+++ README.md
@@ -6,8 +6,16 @@
 - [x] - /api/topics/ GET  
 - [x] - /api/topics/ POST  
 - [x] - /api/topics/{id}/update POST  
-
+- [ ] - OAuth Login (Github)
+- [ ] - Admin Functionality
+	- [ ] Edit Topics
+	- [ ] Archive Topics
+	- [ ] Add Links to topics
+	- [ ] Add Comments to topics
 ## Client
 - [x] - Angular  
 - [x] - Post New Topic  
 - [x] - View Topics  
+- [x] - Angular Material
+- [ ] - View Archived
+- [ ] - Login
--- app.js
+++ app.js
@@ -2,8 +2,32 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+var ClientJWTBearerStrategy = require('passport-oauth2-jwt-bearer').Strategy;
+
 var api = require('./routes/api');
 var apiTopics = require('./routes/apiTopics');
+
+var GITHUB_CLIENT_ID = "--insert-github-client-id-here--";
+var GITHUB_CLIENT_SECRET = "--insert-github-client-secret-here--";
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new ClientJWTBearerStrategy(
+  function(claimSetIss, done) {
+    Clients.findOne({ clientId: claimSetIss }, function (err, client) {
+      if (err) { return done(err); }
+      if (!client) { return done(null, false); }
+      return done(null, client);
+    });
+  }
+));
 
 app.use(express.static('public'));
 
